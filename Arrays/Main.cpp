4e6fd81730c3de722e3fdbfbe1f1e42585d58e56
Vinok_yt86@top-academy.ru <igorvinokurov1984@gmail.com>
2022-11-09 21:37:34 +0300

    Avg last

--- Arrays/Main.cpp
+++ Arrays/Main.cpp
@@ -1,8 +1,55 @@
 #include<iostream>
 using namespace std;
 
+void FillRand(int arr[], const int n, int minRand = 0, int maxRand = 100); // Реализация
+void Print(int arr[], const int n);
+int Sum(int arr[], const int n);
+double Avg(int arr[], const int n);
 
 void main ()
 {
+	setlocale(LC_ALL, "");
+	const int n = 5;
+	int arr[n] = {};
 
+	FillRand(arr, n);
+	Print(arr, n);
+	cout << "Сумма элементов массива: " << Sum(arr, n) << endl;
+	cout << "Среднее арефмитическое элементов массива: " << Avg(arr, n) << endl;
+}
+
+void FillRand(int arr[], const int n, int minRand, int maxRand) // Прототип
+{
+	if (minRand > maxRand)
+	{
+		int buff = minRand; minRand = maxRand; maxRand = buff;
+	}
+	if (minRand == maxRand)maxRand++;
+	//Заполнение случайными числами:
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % (maxRand - minRand) + minRand;
+	}
+}
+void Print(int arr[], const int n)
+{
+	//Вывод массива на экран:
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
+int Sum(int arr[], const int n)
+{
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+	}
+	return sum;
+}
+double Avg(int arr[], const int n)
+{
+	return (double)Sum(arr, n) / n;
 }
