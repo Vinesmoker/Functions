a8c1f2f35b3c7cea00149a3db7f312ceb6ec32be
Vinok_yt86@top-academy.ru <igorvinokurov1984@gmail.com>
2022-11-10 18:38:19 +0300

    HomeWork

--- Arrays/Main.cpp
+++ Arrays/Main.cpp
@@ -5,6 +5,9 @@ void FillRand(int arr[], const int n, int minRand = 0, int maxRand = 100); // Ре
 void Print(int arr[], const int n);
 int Sum(int arr[], const int n);
 double Avg(int arr[], const int n);
+int minValueIn(int arr[], const int n);
+int maxValueIn(int arr[], const int n);
+int shiftLeft(int arr[], const int n);
 
 void main ()
 {
@@ -16,6 +19,9 @@ void main ()
 	Print(arr, n);
 	cout << "Сумма элементов массива: " << Sum(arr, n) << endl;
 	cout << "Среднее арефмитическое элементов массива: " << Avg(arr, n) << endl;
+	cout << "Минимальное значение элемена массива: " << minValueIn(arr, n) << endl;
+	cout << "Максимальное значение элемента в массиве: " << maxValueIn(arr, n) << endl;
+	cout << shiftLeft(arr, n) << endl;
 }
 
 void FillRand(int arr[], const int n, int minRand, int maxRand) // Прототип
@@ -52,4 +58,41 @@ int Sum(int arr[], const int n)
 double Avg(int arr[], const int n)
 {
 	return (double)Sum(arr, n) / n;
+}
+int minValueIn(int arr[], const int n)
+{
+	int min = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min)min = arr[i];
+	}
+	return min;
+}
+int maxValueIn(int arr[], const int n)
+{
+	int max = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > max)max = arr[i];
+	}
+	return max;
+}
+int shiftLeft(int arr[], const int n)
+{
+	cout << "Введите количество сдвигов влево: "; int numOfShifts; cin >> numOfShifts;
+	for (int i = 0; i < numOfShifts; i++)
+	{
+		int buff = arr[0];
+		for (int i = 0; i < n; i++)
+		{
+			arr[i] = arr[i + 1];
+		}
+		arr[n - 1] = buff;
+		for (int i = 0; i < n; i++)
+		{
+			cout << arr[i] << "\t";
+		}
+		return arr[i];
+	}
+
 }
