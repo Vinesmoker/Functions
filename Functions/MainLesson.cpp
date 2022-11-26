ac516ba364205087d2c2463e2cfb7a4a05e392c5
Vinok_yt86@top-academy.ru <igorvinokurov1984@gmail.com>
2022-11-26 16:54:01 +0300

    Power

--- Functions/MainLesson.cpp
+++ Functions/MainLesson.cpp
@@ -4,19 +4,21 @@ using namespace std;
 int Add(int a, int b);
 int Sub(int a, int b);
 int Mul(int a, int b);
-int Div(int a, int b);
+double Div(double a, int b);
+double Pow(double a, int b, int buff, double c);
 
 void main()
 {
 	setlocale(LC_ALL, "");
-	int a, b;
+	double a; int buff = 0; double c = 0; int b;
 	cout << "Hello Functions!!" << endl;
 	cout << " ¬ведите 2 числа: "; cin >> a >> b;
-	int c = Add(a, b);
-	cout << a << " + " << b << " = " << c << endl;
+	int s = Add(a, b);
+	cout << a << " + " << b << " = " << s << endl;
 	cout << a << " - " << b << " = " << Sub(a, b) << endl;
 	cout << a << " * " << b << " = " << Mul(a, b) << endl;
 	cout << a << " / " << b << " = " << Div(a, b) << endl;
+	cout << a << " To power " << b << " = " << Pow(a, b, buff, c) << endl;
 }
 int Add(int a, int b)
 {
@@ -31,7 +33,20 @@ int Mul(int a, int b)
 {
 	return a * b;
 }
-int Div(int a, int b)
+double Div(double a, int b)
 {
 	return a / b;
+}
+double Pow(double a, int b, int buff, double c)
+{
+	if (b == 0)a = 1;
+	if (b > 0)buff = b;
+	if (b < 0)buff = b * (-1);
+	c = a;
+	for (int i = 1; i < buff; i++)
+	{
+		c = c * a;
+	}
+	if (b > 0) return c;
+	else return 1 / c;
 }
